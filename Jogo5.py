c8ce4acbf90786dc816afabad50664fdfa6d5f49
cassiolira <cassiolira@gmail.com>
2022-01-15 22:46:42 -0300

    Initial Commit

--- Jogo5.py
+++ Jogo5.py
@@ -8,30 +8,39 @@ import random as rd
 # rd.random() gera um numero de 0 a 1, mas podemos utilizar a função rd.randgange(0,101)
 numero_secreto = int(rd.random()*100)
 
-tentativas_str = input('How many attempts do you want?')
-print('You chose:', tentativas_str)
-print('You will have', tentativas_str, 'chances!')
-# È necessário converter o tentativas_str, que é uma variável string, numa variável integer, neste caso tentativas_int,
-# usando a função INT
-tentativas_int = int(tentativas_str)
+# Vamos incluir uma seleção de dificuldades do jogo, escolhida pelo Usuário
+
+level_str = input('Please, select your level: Type (1) for Easy, (2) for Normal or (3) for Master Level')
+level = int(level_str)
+if (level == 1):
+    print('You selected te Easy Level')
+    tentativas = 12
+elif (level == 2):
+    print('You selected te Normal Level')
+    tentativas = 8
+elif (level == 3):
+    print('You selected te Master Level')
+    tentativas = 5
+else:
+    print('Wrong number!')
+
 
 # Início do código do jogo.
-for rodadas in range(1, tentativas_int):
-    print('Try', rodadas, 'of', tentativas_int)
+for rodadas in range(1, tentativas + 1):
+    print('Try', rodadas, 'of', tentativas)
     chute_str = input('Enter a number between 0 to 100:')
-    print('You typed:: ', chute_str)
-    # È necessário converter o chute_str, que é uma variável string, numa variável integer, neste caso chute_int,
-    # usando a função INT
-    chute_int = int(chute_str)
-    if (0 > chute_int or chute_int > 100):
+    chute = int(chute_str)
+    print('You typed: ', chute)
+
+    if (0 > chute or chute > 100):
         print('Please, out of range, choose a number between 0 to 100!')
         continue
     else:
-        if (numero_secreto == chute_int):
+        if (numero_secreto == chute):
             print('Congratulation! You got it!')
             break
         else:
-            if (numero_secreto < chute_int):
+            if (numero_secreto < chute):
                 print('You lose! Your number is bigger than the secret number!')
             else:
                 print('You lose! Your number is smaller than the secret number!')
