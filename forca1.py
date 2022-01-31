9e4af594576e40fba910ca1f3ff77f580d7425b6
cassiolira <cassiolira@gmail.com>
2022-01-31 20:34:35 -0300

    Initial Commit

--- forca1.py
+++ forca1.py
@@ -1,10 +1,21 @@
+import random
+
 def jogar():
 
     print('****Forca Game!****')
-    #palavra_secreta não é uma list e sim um tuple, é imutável, por isso usamos parenteses
-    #ao inves de colchete
-    palavra_secreta = ('PYTHON')
-    letras_acertadas = ['_','_','_','_','_','_']
+
+    #Geração de uma lista randomica.
+    
+    arquivo = open('palavras.txt')
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
@@ -19,10 +30,8 @@ def jogar():
             index = 0
             for letra in palavra_secreta:
                 if(chute == letra):
-                    print('Você advinhou a letra:',chute,'na posição', index + 1)
                     letras_acertadas[index] = letra
-                    print(letras_acertadas)
-                    print('Continue jogando...')
+                    print('Tem a letra', chute, 'na posição', index + 1)
                 index = index + 1
         else:
             erros = erros + 1
